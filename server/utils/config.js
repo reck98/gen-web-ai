9b311f63558ff467e04cf6ac95de17decd8fc80d
reck98 <aashish7648@gmail.com>
2026-06-25 22:45:19 +0530

    open router configured

--- server/utils/config.js
+++ server/utils/config.js
@@ -6,3 +6,6 @@ export const MONGO_URI = process.env.MONGO_URI;
 export const DB_NAME = process.env.DB_NAME || "gen-web-ai-db";
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
+export const OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions";
+export const MODEL_NAME = "deepseek/deepseek-chat";
---
+++ server/utils/openRouter.js
@@ -0,0 +1,35 @@
+import { MODEL_NAME, OPENROUTER_API_KEY, OPENROUTER_URL } from "./config.js";
+
+const generateRespone = async () => {
+    const response = await fetch(OPENROUTER_URL, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${OPENROUTER_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: MODEL_NAME,
+            messages: [
+                {
+                    role: "system",
+                    content: "You must return ONLY valid raw JSON",
+                },
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.2,
+        }),
+    });
+
+    if (!response.ok) {
+        const error = await response.text();
+
+        throw new Error(`OpenRouter Error : ${error}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+};
